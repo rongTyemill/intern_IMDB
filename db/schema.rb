4df566656f9ff9439441fee15ca445a3f91a6b90
RongTyemill <rong@tyemill.com>
2020-09-24 10:49:02 -0700

    Add user to review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_24_154738) do
+ActiveRecord::Schema.define(version: 2020_09_24_171606) do
 
   create_table "models", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_09_24_154738) do
     t.text "comment"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
